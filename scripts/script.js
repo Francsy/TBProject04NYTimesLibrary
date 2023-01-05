1486cc1e30814e29ef8bc49815b3a8c6d41c1666
Francsy <francsyludens@gmail.com>
2023-01-05 12:11:57 +0100

    add/función que pinta la lista principal sin implementar animación ni funcionalidad de los botones de cada libro

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,20 @@ const getDataLists = async () => {
     const mainListArray = dataMainList.results;
     return mainListArray;
 }
+//Función para pintar en el DOM la lista principal:
+const createMainList = async () => {
+    //función animación
+    const listsArray = await getDataLists();
+    const section = document.getElementById('list');
+    section.innerHTML = '<h1>MAIN LIST</h1>';
+    listsArray.forEach(list => {
+        const cardDiv = document.createElement('div')
+        cardDiv.innerHTML =`<h2>${list.display_name}</h2><p>Oldest book: ${list.oldest_published_date}</p><p>Newest book: ${list.newest_published_date}</p><p>Updated: ${list.updated}</p>`
+        section.appendChild(cardDiv);
+        const cardButton = document.createElement('button');
+        cardButton.innerHTML = 'BRING ME IN!';
+        cardButton.setAttribute('type', 'button');
+        // cardButton.onclick = //funcion list.list_name_encoded
+        section.appendChild(cardButton);
+    });
+}
