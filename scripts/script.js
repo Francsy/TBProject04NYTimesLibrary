eddd864b842ee72abe566832839e8217dea03f07
Francsy <francsyludens@gmail.com>
2023-01-05 13:23:24 +0100

    Add funcion extractora de datos de la API y función que usa dichos datos para pintar la estructura completa de cada lista especifica

--- scripts/script.js
+++ scripts/script.js
@@ -23,7 +23,29 @@ const createMainList = async () => {
         const cardButton = document.createElement('button');
         cardButton.innerHTML = 'BRING ME IN!';
         cardButton.setAttribute('type', 'button');
-        // cardButton.onclick = //funcion list.list_name_encoded
+        cardButton.addEventListener('click', ()=> createBooksList(list.list_name_encoded));
         section.appendChild(cardButton);
     });
 }
+//Función que extrae lista específica de libros y construye objeto necesario para pintar
+const getDataBooks = async (code) => {
+    const rawDataBooks = await fetch(`https://api.nytimes.com/svc/books/v3/lists/current/${code}.json?api-key=hksRlq4zXFu3Itqjruc8igFoj22s4ZRR`)
+    const dataBooks = await rawDataBooks.json();
+    const booksList = dataBooks.results;
+    return booksList; 
+}
+//Función que pinta en el DOM lista específica de libros
+const createBooksList = async (code) => {
+    //función animación
+    const booksList = await getDataBooks(code);
+    const section = document.getElementById('list');
+    section.innerHTML = `<h1>${booksList.list_name}</h1><button id="back-button" type="button">&#60 BRING ME BACK!</button>`;
+    booksList['books'].forEach(book => {
+        const cardDiv = document.createElement('div');
+        cardDiv.innerHTML = `<h2>#${book.rank} ${book.title}</h2><img src="${book.book_image}" alt="book cover"><p>Weeks on list: ${book.weeks_on_list}</p><p>${book.description}</p><a href="${book.amazon_product_url}" target="_blank"><button>BUY!</button></a>`
+        section.appendChild(cardDiv)
+    })
+    const backButton = document.getElementById('back-button');
+    backButton.onclick = createMainList;
+}
+
